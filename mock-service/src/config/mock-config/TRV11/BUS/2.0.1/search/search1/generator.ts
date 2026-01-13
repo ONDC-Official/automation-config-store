d18230cad2cca5a34d6e296857098102de0801b8
Shivang Dudani <shivangdudani113@gmail.com>
2026-01-13 12:51:51 +0530

    adding bpp_uri as input

--- mock-service/src/config/mock-config/TRV11/BUS/2.0.1/search/search1/generator.ts
+++ mock-service/src/config/mock-config/TRV11/BUS/2.0.1/search/search1/generator.ts
@@ -23,7 +23,6 @@ export async function searchGenerator(
   existingPayload: any,
   sessionData: SessionData
 ) {
-  existingPayload.context.bpp_uri = sessionData.subscriber_url;
   const stops = existingPayload.message.intent.fulfillment.stops;
   stops[0].location.descriptor.code = "start_station";
   stops[1].location.descriptor.code = "end_station";
